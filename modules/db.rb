b3dfc2e88129aa19584af5826ba48d58280dd4fa
Fernando Romero <fernando.romero@rightscale.com>
2013-09-22 22:00:13 -0500

    rake db tasks updated

--- modules/db.rb
+++ modules/db.rb
@@ -5,24 +5,27 @@ module Tradenetic
 
     def self.create_database
       client = Mysql2::Client.new(:host => "localhost", :username => "root")
-      query = 'CREATE DATABASE IF NOT EXISTS tradenetic;'
+      query = 'DROP DATABASE tradenetic;'
+      client.query(query)
+      query = 'CREATE DATABASE tradenetic;'
       client.query(query)
     end
 
     def self.create_tables
       client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => 'tradenetic')
       query = <<-sql
-        CREATE TABLE IF NOT EXISTS targets (
+        CREATE TABLE IF NOT EXISTS target (
           id INT NOT NULL AUTO_INCREMENT,
           symbol VARCHAR(30) NOT NULL,
           type CHAR(3) NOT NULL,
           timeframe VARCHAR(3) NOT NULL,
-          PRIMARY KEY (id)
+          PRIMARY KEY (id),
+          UNIQUE (symbol, type, timeframe)
         );
       sql
       client.query(query)
       query = <<-sql
-        CREATE TABLE IF NOT EXISTS bars (
+        CREATE TABLE IF NOT EXISTS bar (
           id INT NOT NULL AUTO_INCREMENT,
           target_id INT NOT NULL,
           open DECIMAL(13, 2),
@@ -32,7 +35,8 @@ module Tradenetic
           volume INT,
           time TIMESTAMP,
           PRIMARY KEY (id),
-          FOREIGN KEY (target_id) REFERENCES targets(id)
+          UNIQUE (target_id, time),
+          FOREIGN KEY (target_id) REFERENCES target(id)
         );
       sql
       client.query(query)
@@ -41,8 +45,15 @@ module Tradenetic
     def self.add_initial_targets
       client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => 'tradenetic')
       query = <<-sql
-        INSERT INTO targets (symbol, type, timeframe)
-        VALUES ('AAPL', 'STK', '1M');
+        INSERT INTO target (symbol, type, timeframe) VALUES ('AAPL', 'STK', '1M');
+      sql
+      client.query(query)
+      query = <<-sql
+        INSERT INTO target (symbol, type, timeframe) VALUES ('GOOG', 'STK', '1M');
+      sql
+      client.query(query)
+      query = <<-sql
+        INSERT INTO target (symbol, type, timeframe) VALUES ('MSFT', 'STK', '1M');
       sql
       client.query(query)
     end
